79edf6ed7906cff6d67834c3fbd049717c8e79b8
Terry (Yan) Wang <enjoybusy@hotmail.com>
2019-07-29 22:09:07 +1000

    Refactor RabbitMQ Bus for Dependency Injection for parameterless constructors in our event handlers

--- MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MicroRabbit.Domain.Core.Bus
 {
-    public interface IEventHandler<in TEvent> : IEventHandler 
+    public interface IEventHandler<in TEvent> : IEventHandler
         where TEvent : Event
     {
         Task Handle(TEvent @event);
